bda9650ec4dec8927564e7cabe933ec09064fb99
veelo <veelo@88dfee38-930f-0410-ae76-ff5e2cf808d5>
2006-04-28 17:43:21 +0000

    Initial versions as supplied by Gerrie Thiart on April 28 2006.

---
+++ windows/KindControl.f95
@@ -0,0 +1,12 @@
+! Control settings for variable "kind" control [Origin: Package Ellipsoid]
+MODULE KindControl
+  IMPLICIT NONE
+! Interfaces  
+! Global parameters
+  INTEGER, PARAMETER :: intKind = 3, realKind = 3
+! Types
+! Global variables
+  
+CONTAINS   
+ 
+END MODULE KindControl
